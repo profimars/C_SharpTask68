529af4f34968dc85fb1055164b2cc98e124d149e
Марс <profimars@mail.ru>
2023-04-02 21:44:30 +0300

    Добавили задачу C_SharpTask68

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+//https://github.com/profimars/C_SharpTask68
+
+int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return A(m - 1, 1);
+    else
+        return A(m - 1, A(m, n - 1));
+}
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A(m, n));
